49dc0171e2ce6e6df4f10f68abb3abc003ef0361
seamussmith <student.141504@worcesterschools.net>
2021-11-18 08:51:08 -0500

    wah

--- src/main/App.java
+++ src/main/App.java
@@ -4,6 +4,9 @@ public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        Beverage beverage = new DarkRoast();
+        beverage = new MilkCondiment(beverage);
+        System.out.println(beverage.getDescription());
+        System.out.println(beverage.cost());
     }
 }
--- src/main/CondimentDiscount.java
+++ src/main/CondimentDiscount.java
@@ -1,22 +1,23 @@
 package main;
 
-public class Rabbit extends CondimentDecorator {
+public class CondimentDiscount extends CondimentDecorator {
 
-    Rabbit(Beverage beverage) {
+    double discount;
+    CondimentDiscount(Beverage beverage, double discount) {
         super(beverage);
-        //TODO Auto-generated constructor stub
+        this.discount = discount;
     }
 
     @Override
     public String getDescription() {
         // TODO Auto-generated method stub
-        return "Rabbit...?";
+        return "With a " + discount + "% discount";
     }
 
     @Override
     public double cost() {
         // TODO Auto-generated method stub
-        return beverage.cost() + 5000;
+        return 0;
     }
     
 }
